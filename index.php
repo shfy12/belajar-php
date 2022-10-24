76ac332f56f64b6d6187337d55272179c4899546
Shofiyyah Az zahra <ashofiyyah12@gmail.com>
2022-10-24 18:02:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello world!<br>";
+
+$nama = "Shofiyyah";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaadik = "Ilyas";
+$umuradik = 15;
+
+echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun.<br>";
+
+$selisihumur = $umur - $umuradik;
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
+
+?>
