80e34493324e4e086bd0fa71bcbb4e7fe32ca5f5
Shofiyyah Az zahra <ashofiyyah12@gmail.com>
2022-11-24 08:48:39 +0700

    crud belum selesai

--- index.php
+++ index.php
@@ -1,18 +1,56 @@
 <?php
+//1. buat koneksi dengan MySQL
+$con = mysqli_connect("localhost","root","","seal_fakultas")
 
-echo "Hello world!<br>";
+//2. cek koneksi dengan MySQL
+if (mysqli_connect_errno()) {
+    echo "koneksi gagal:" . mysqli_connect_error();
+    exit();
+} else{
+    echo 'koneksi berhasil';
+}
 
-$nama = "Shofiyyah";
-$umur = 20;
+//3. membaca data dari table mysql
+$query = "SELECT * FROM mahasiswa";
 
-echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+//4. menampilkan data dengan sql query
+$result = mysqli_query($con,$query)
+if($result){
+    //menampilkan data satu per satu
+    /*
+    $row = mysqli_fetch_assoc($result);
+    var_dump($row);
+    echo $row ["nama"]
+    */
+    while($row=mysqli_fetch_assoc($result)){
+        echo "<br>". $row["nama"]."alamat : ".$row["alamat"];
+    }
+    mysqli_free_result($result);
+}
 
-$namaadik = "Ilyas";
-$umuradik = 15;
-
-echo "Nama adik saya <strong>$namaadik</strong>, adik saya berusia $umuradik tahun.<br>";
-
-$selisihumur = $umur - $umuradik;
-echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
-
+//5.tutup koneksi mysql
+mysqli_close($con);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <tr>
+            <td>21112022</td>
+            <td>Melati</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,33 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $id_jurusan = $_POST['id_jurusan'];
+    echo $nama;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    <form action="" method="post">
+        NIM: <input type="text" name="nim"><br>
+        Nama: <input type="text" name="nama"><br>
+        ID Jurusan: <input type="number" name="id_jurusan"><br>
+        Jenis Kelamin: <input type="text" name="jenis kelamin"><br>
+        Tempat Lahir: <input type="text" name="tpt_lahir"><br>
+        Tanggal Lahir (yyyy-mm-dd): <input type="text" name="tgl_lahir"><br>
+        Alamat: <input type="text" name="alamat"><br>
+        <button type="submit" name="submit">Tambah Data</button>
+    </form>
+</body>
+</html>
